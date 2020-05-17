1f40c9f422894097297725e1e92170a045bc61f6
KaanHGunay <kaangunay16@gmail.com>
2020-05-17 12:19:58 +0300

    Widget ayırma

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,30 @@
+import 'package:Flutter/quiz.dart';
+import 'package:Flutter/result.dart';
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favourite color?',
+      'answers': ['Black', 'Red', 'Green', 'White']
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers': ['Cat', 'Dog', 'Bird', 'Snake']
+    },
+    {
+      'questionText': 'Who\'s your favourite man?',
+      'answers': ['He', 'She', 'We', 'They']
+    }
+  ];
+
   int _questionIndex = 0;
 
   void _answerQuestion() {
@@ -22,30 +36,15 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {'questionText': 'What\'s your favourite color?', 'answers': [
-        'Black', 'Red', 'Green', 'White'
-      ]},
-      {'questionText': 'What\'s your favourite animal?', 'answers': [
-        'Cat', 'Dog', 'Bird', 'Snake'
-      ]},
-      {'questionText': 'Who\'s your favourite man?', 'answers': [
-        'He', 'She', 'We', 'They'
-      ]}
-    ];
-
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-            title: Text('My First Page')
-        ),
-        body: Column(children: <Widget>[
-          Question(questions[_questionIndex]['questionText']),
-          ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
-            return Answer(_answerQuestion, answer);
-          })
-        ],
-        ),
+        appBar: AppBar(title: Text('My First Page')),
+        body: _questionIndex < questions.length
+            ? Quiz(
+                questions: this.questions,
+                questionIndex: this._questionIndex,
+                answerQuestion: this._answerQuestion)
+            : Result(),
       )
     );
   }
